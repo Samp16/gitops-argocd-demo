0a5a153132a503709ddb2a51b90b71f065836959
Samhitha P <samhitha.p@planview.com>
2025-04-08 14:47:45 +0530

    Initial commit: React frontend app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+  🚀 Welcome to My GitOps React App!
+</p>
+<a
+  className="App-link"
+  href="https://github.com/Samp16"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Visit My GitHub
+</a>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
