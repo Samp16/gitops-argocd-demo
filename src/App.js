21c51d610156ebe0dcc51428bcd69cb6b9bea928
Samhitha P <samhitha.p@planview.com>
2025-04-14 00:14:41 +0530

    Update app title for ArgoCD demo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-  🚀 Welcome to My GitOps React App!
+  🚀 Welcome Deployed with ArgoCD GitOps
 </p>
 <a
   className="App-link"
